5cbd7ce5539f68991cef6a9baffae7b81a7a3033
Lian <lianmaher73@gmail.com>
2025-12-23 22:53:14 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,22 +1,17 @@
 self.addEventListener("notificationclick", function (event) {
   event.notification.close();
   const type = event.notification.data.type;
-
-  // بناء الرابط الكامل للتوجيه
-  const targetUrl = new URL(`./index.html?type=${type}`, self.location.origin)
-    .href;
+  const targetUrl = `./index.html?type=${type}`;
 
   event.waitUntil(
     clients
       .matchAll({ type: "window", includeUncontrolled: true })
       .then((windowClients) => {
-        // إذا كان التطبيق مفتوحاً بالفعل، قم بتحديث الرابط والتركيز عليه
         for (let client of windowClients) {
-          if (client.url.includes("index.html") && "focus" in client) {
+          if ("navigate" in client) {
             return client.navigate(targetUrl).then((c) => c.focus());
           }
         }
-        // إذا كان مغلقاً، افتح نافذة جديدة
         if (clients.openWindow) {
           return clients.openWindow(targetUrl);
         }
