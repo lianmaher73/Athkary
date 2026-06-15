ef439e4831b48b75c7a91460fb412fed9f599fb8
Lian <lianmaher73@gmail.com>
2026-06-15 07:05:06 +0300

    fix download problem

--- sw.js
+++ sw.js
@@ -1,28 +1,7 @@
-const CACHE_NAME = "ajr-cache-v2";
-
-// تثبيت السيرفس ووركر
-self.addEventListener("install", e => {
-  e.waitUntil(self.skipWaiting());
-});
-
-// تفعيله فوراً
-self.addEventListener("activate", e => {
-  e.waitUntil(self.clients.claim());
-});
-
-// كاش أساسي للملفات
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(cached => {
-      return cached || fetch(e.request);
-    })
-  );
-});
 self.addEventListener("notificationclick", function (event) {
   event.notification.close();
   const type = event.notification.data.type;
   const targetUrl = `./index.html?type=${type}`;
-  const CACHE_NAME = "ajr-cache-v2"; // غيّر الرقم كل مرة تحدّث
 
   event.waitUntil(
     clients
